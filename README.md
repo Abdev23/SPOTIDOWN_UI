65e0a9c0df0157de587113756193638cb44cd8ec
Abdev23 <agadir.9ine98@gmail.com>
2023-11-04 02:13:55 +0100

    2-some change on the read me and Why component

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
-# React + Vite
+# SpotiDown_UI
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+SpotiDown is a web application that uses the Spotify API to fetch music metadata. It features a clean and simple user interface, supports dark/light mode, and many features to come in the future.
 
-Currently, two official plugins are available:
+## Features
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- **Spotify API Integration:** SpotiDown uses the Spotify API to fetch music metadata, including track information, artists, and album details.
+
+- **Dark/Light Mode Support:** The application provides users with the option to switch between dark and light modes, enhancing user experience based on their preference.
+
+- **Offline Page:** SpotiDown offers an offline page with appropriate messaging and instructions in case the user's internet connection is lost.
+
+- **Simple and Clean UI:** The user interface is designed to be intuitive and user-friendly, allowing users to easily search for music and view relevant information.
+
+## Contributors
+
+- [Abdelhamid](https://github.com/Abdev23) - Developer
+
+## License
+
+This project is licensed under the [MIT License](README.md).
--- src/components/Why/Why.jsx
+++ src/components/Why/Why.jsx
@@ -7,14 +7,16 @@ import './Why.css';
 const Why = () => {
 
   return (
-    <div className='section column' id='why' data-astro-cid-j7pv25f6=''>
-      <h2 data-astro-cid-j7pv25f6='' style={{fontFamily: 'Poppins, sans-serif'}}>
+    <div className='section column'
+         id='why'
+    >
+      <h2>
         Why SpotiDown?
       </h2>
       
-      <div className='section-cards' data-astro-cid-j7pv25f6=''>
-        <div className='section-card' data-astro-cid-j7pv25f6=''>
-          <svg className='section-card-img' data-astro-cid-j7pv25f6=''
+      <div className='section-cards' >
+        <div className='section-card' >
+          <svg className='section-card-img' 
                width='117' height='117'
                viewBox='0 0 117 117'
                fill='none'
@@ -22,76 +24,71 @@ const Why = () => {
           >
             <circle cx='58.5' cy='58.5'
                     r='58.5' fill='var(--bg)'
-                    data-astro-cid-j7pv25f6=''
+                    
             ></circle>
             <path fillRule='evenodd' clipRule='evenodd'
                   d='M87.8217 7.86778C88.7499 8.40648 89.0469 9.59549 88.5152 10.5277L58.9095 62.429C58.4148 63.2962 57.3564 63.6658 56.4294 63.295L4.14483 42.3812C3.18375 41.9968 2.67153 40.9403 2.99938 39.9584C10.7533 16.7379 32.672 0 58.5 0C69.185 0 79.2009 2.86463 87.8217 7.86778Z'
                   fill='var(--accent)'
                   fillOpacity='0.6'
-                  data-astro-cid-j7pv25f6=''
+                  
             ></path>
             <path d='M81.5 22.5L57.1395 64.8489L32 53.5'
                   stroke='var(--text)'
                   strokeWidth='8'
                   strokeLinecap='round'
                   strokeLinejoin='round'
-                  data-astro-cid-j7pv25f6=''
+                  
             ></path>
           </svg>
           
-          <h4 className='highlight' data-astro-cid-j7pv25f6=''
-              style={{fontFamily: 'Poppins, sans-serif'}}
-          >
+          <h4 className='highlight'>
             Saves time
           </h4>
-          <p data-astro-cid-j7pv25f6='' style={{fontFamily: 'Poppins, sans-serif'}}>
+          <p>
             No need to spend hours implementing different variations of colors. Decide right away!
           </p>
         </div>
 
-        <div className='section-card' data-astro-cid-j7pv25f6=''>
-          <svg className='section-card-img' data-astro-cid-j7pv25f6=''
+        <div className='section-card'>
+          <svg className='section-card-img' 
                width='112' height='114'
                viewBox='0 0 112 114'
                fill='none' xmlns='http://www.w3.org/2000/svg'
           >
             <rect width='58' height='58'
                   rx='5' fill='var(--bg)'
-                  data-astro-cid-j7pv25f6=''
+                  
             ></rect>
             <rect x='69' y='25'
                   width='33' height='33'
                   rx='5'
                   fill='var(--accent)'
                   fillOpacity='0.6'
-                  data-astro-cid-j7pv25f6=''
+                  
             ></rect>
             <rect x='69' y='71'
                   width='43' height='43' rx='5'
                   fill='var(--text)'
                   fillOpacity='0.1'
-                  data-astro-cid-j7pv25f6=''
+                  
             ></rect>
             <rect x='20' y='70'
                   width='38' height='39'
                   rx='5' fill='var(--text)'
-                  data-astro-cid-j7pv25f6=''
+                  
             ></rect>
           </svg>
           
-          <h4 className='highlight' data-astro-cid-j7pv25f6=''
-              style={{fontFamily: 'Poppins, sans-serif'}}
-          >
+          <h4 className='highlight'>
             It’s Realistic
           </h4>
-          <p data-astro-cid-j7pv25f6='' style={{fontFamily: 'Poppins, sans-serif'}}>
+          <p>
             Color Palettes make it hard to pick. This tool distributes the colors on a real website.
           </p>
         </div>
 
-        <div className='section-card' data-astro-cid-j7pv25f6=''
-        >
-          <svg className='section-card-img' data-astro-cid-j7pv25f6=''
+        <div className='section-card'>
+          <svg className='section-card-img' 
                width='178' height='89'
                viewBox='0 0 178 89'
                fill='none'
@@ -101,34 +98,31 @@ const Why = () => {
                   width='154' height='63'
                   rx='5' fill='var(--accent)'
                   fillOpacity='0.6'
-                  data-astro-cid-j7pv25f6=''
+                  
             ></rect>
             <path d='M142 16L142 4' stroke='var(--text)'
                   strokeWidth='8' strokeLinecap='round'
-                  data-astro-cid-j7pv25f6=''
+                  
             ></path>
             <path d='M163 34H174' stroke='var(--text)'
                   strokeWidth='8' strokeLinecap='round'
-                  data-astro-cid-j7pv25f6=''
+                  
             ></path>
             <path d='M158 19L168 9' stroke='var(--text)'
                   strokeWidth='8' strokeLinecap='round'
-                  data-astro-cid-j7pv25f6=''
+                  
             ></path>
             <path d='M62 57L73.5 68L94.5 47' stroke='var(--text)'
                   strokeWidth='8' strokeLinecap='round'
                   strokeLinejoin='round'
-                  data-astro-cid-j7pv25f6=''
+                  
             ></path>
           </svg>
           
-          <h4 className='highlight'
-              data-astro-cid-j7pv25f6=''
-              style={{fontFamily: 'Poppins, sans-serif'}}
-          >
+          <h4 className='highlight'>
             It’s simple
           </h4>
-          <p data-astro-cid-j7pv25f6='' style={{fontFamily: 'Poppins, sans-serif'}}>
+          <p>
             Push a few buttons, and there you have it! Your very own branding colors, ready to export.
           </p>
         </div>
