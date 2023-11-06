179624a4e6642a44b5ef8b1acaccfac812971e9b
Abdev23 <agadir.9ine98@gmail.com>
2023-11-06 21:21:35 +0100

    11-adding the FAQ component

---
+++ src/components/Faq/FaqData.jsx
@@ -0,0 +1,38 @@
+
+const FaqData = [
+  {
+    question: `What is SpotiDown?`,
+    answer: `It's a simple tool that allows you to easily download your favorite tracks, albums, or playlists from Spotify without the need to purchase a premium subscription.`
+  },
+  {
+    question: `How does SpotiDown work?`,
+    answer: `SpotiDown works by extracting the audio files from Spotify and converting them into a downloadable format. Users simply need to copy and paste the URL of the Spotify song, album, or playlist they wish to download into the SpotiDown website, and the site will handle the rest.`
+  },
+  {
+    question: `Is SpotiDown free to use?`,
+    answer: `Yes, SpotiDown is completely free to use. There are no hidden fees or charges, and users can download as many songs, albums, or playlists as they want.`
+  },
+  {
+    question: `Is SpotiDown legal?`,
+    answer: `Yes, downloading Spotify songs for personal use is legal. However, if you intend to use the downloaded content for any other purpose, you must purchase the necessary rights from the owner.`
+  },
+  {
+    question: `Is SpotiDown safe to use?`,
+    answer: `Our website is completely safe to use, and you can expect to receive only what you intended. We regularly update our tool to ensure it functions properly and to mitigate security risks.`
+  },
+  {
+    question: `What file formats does SpotiDown support?`,
+    answer: `SpotiDown supports a variety of file formats, including MP3, AAC, M4A and FLAC.`
+  },
+  {
+    question: `Can I use SpotiDown on any operating system?`,
+    answer: `Yes, SpotiDown works on any operating system, including Windows, Mac, and Linux. As long as you have a web browser, you can use SpotiDown to download music from Spotify.`
+  },
+  {
+    question: `Can I use SpotiDown on my mobile device?`,
+    answer: `Yes, SpotiDown can be used on mobile devices as well. Simply visit the SpotiDown website using your mobile browser and follow the same steps to download music from Spotify.`
+  }
+];
+
+
+export default FaqData;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
         <Testimonials />
         <Faq />
         <FeaturedArticles />
-        <Cta />
+        {/* <Cta /> */}
       </main>
     </div>
   )
